4a1f9ad383202f27d3ab5a4fa082c0e7cd8201b7
Ben Ferraro <Ben@dhcp490-175.umiacs.umd.edu>
2015-07-06 13:55:53 -0400

    Submit Client

---
+++ GarliClient.java
@@ -0,0 +1,43 @@
+
+
+
+public class GarliSubmit {
+
+	private static String jobname;
+
+	// private static File propertiesfile;
+
+	public static void main(String [] args) {
+		
+		if (args.length != 2) {
+			System.err.println("Requires 2 arguments: properties file, and job name.");
+            System.exit(1);
+		}
+
+		String propertiesfile = args[0];
+		jobname = args[1];
+
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	} // end main function
+
+
+
+}
