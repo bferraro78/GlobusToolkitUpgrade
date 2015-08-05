c048e043f30417c536dc7972a5c563166ab93539
Jordan Kiesel <jtkiesel@umiacs.umd.edu>
2015-08-05 19:39:04 -0400

    Fixed last push issue.

--- GT6Upgrade/GT4.2/garli/trunk/globus-deploy-garli.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd $GLOBUS_LOCATION
-cd -
-globus-stop-container-detached
-globus-undeploy-gar edu_umd_grid_bio_garli
-globus-deploy-gar edu_umd_grid_bio_garli.gar
-cd -
-./start-stop start
-cd -
--- GT6Upgrade/GT4.2/structure/trunk/edu/umd/grid/bio/structure/impl/StructureFactoryService.java
+++
@@ -1,9 +0,0 @@
-package edu.umd.grid.bio.structure.impl;
-
-import edu.umd.umiacs.cummings.GSBL.GSBLFactoryService;
-
-public class StructureFactoryService extends GSBLFactoryService {
-	
-	// Despite the fact that this class is empty, it seems necessary to preserve the one-to-one factory/instance service model.
-	
-}
