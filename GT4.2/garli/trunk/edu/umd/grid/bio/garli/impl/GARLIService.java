bc2df039368b6a4441559c1ee1a9d6d3ac87d1b2
gt4admin <gt4admin@lysine>
2015-08-13 14:27:25 -0400

    Removed file://

--- GT4.2/garli/trunk/edu/umd/grid/bio/garli/impl/GARLIService.java
+++ GT4.2/garli/trunk/edu/umd/grid/bio/garli/impl/GARLIService.java
@@ -500,7 +500,7 @@ public class GARLIService extends GSBLService {
 			globus_command += (" -f rslString" + unique_id);  // Add RSL file.
 
 			// Move emptyDir and rename it to the job's unique_id.
-			GSBLUtils.executeCommand("globus-url-copy -cd -r gsiftp://" + job.getHost() + job.getWorkingDir() + "emptyDir/ file://${HOME}/" + unique_id + "/");
+			GSBLUtils.executeCommand("globus-url-copy -cd -r gsiftp://" + job.getHost() + job.getWorkingDir() + "emptyDir/ ${HOME}/" + unique_id + "/");
 
 			/* Executes a globusrun command */
 			GSBLUtils.executeCommand(globus_command);
